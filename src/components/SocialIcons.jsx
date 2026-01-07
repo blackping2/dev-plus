8f7509eb46585d9da716a830712980e87ed8ca4b
blackping2 <zx43652@gmail.com>
2026-01-07 17:21:41 +0900

    Create login UI

---
+++ src/components/SocialIcons.jsx
@@ -0,0 +1,15 @@
+export default function SocialIcons() {
+  return (
+    <div className="socialRow">
+      <button type="button" className="socialBtn" aria-label="facebook">
+        f
+      </button>
+      <button type="button" className="socialBtn" aria-label="whatsapp">
+        w
+      </button>
+      <button type="button" className="socialBtn" aria-label="telegram">
+        t
+      </button>
+    </div>
+  );
+}
